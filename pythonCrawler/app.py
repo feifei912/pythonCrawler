42e096ca8f92ae59c4a5170b4b9ea43ff4f84035
feifei <1727906692@qq.com>
2024-12-16 22:17:43 +0800

    唉

--- pythonCrawler/app.py
+++ pythonCrawler/app.py
@@ -1,51 +1,77 @@
-# app.py
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
+import os
+import threading
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from bilibili_covers_fetcher import BilibiliCoversFetcher
-from github_trending_fetcher import GitHubTrendingFetcher, TRENDING_URLS
 from sina_news_fetcher import SinaNewsFetcher
+from github_trending_fetcher import GitHubTrendingFetcher, TRENDING_URLS
+from bilibili_covers_fetcher import BilibiliCoversFetcher
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
-def create_driver():
+
+def run_sina_news_fetcher(option, pages=5):
     chrome_options = Options()
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     driver = webdriver.Chrome(options=chrome_options)
-    return driver
 
-@app.route('/scrape/github', methods=['GET'])
-def scrape_github():
-    time_range = request.args.get('time_range', 'weekly')
-    github_fetcher = GitHubTrendingFetcher()
-    projects = github_fetcher.get_github_trending(TRENDING_URLS[time_range])
-    return jsonify(projects)
+    parent_directory = os.path.dirname(os.path.abspath(__file__))
+    folder_name = os.path.join(parent_directory, 'News')
+    os.makedirs(folder_name, exist_ok=True)
 
-@app.route('/scrape/bilibili', methods=['GET'])
-def scrape_bilibili():
-    video_type = request.args.get('video_type', 'history')
-    driver = create_driver()
-    bilibili_fetcher = BilibiliCoversFetcher(driver, 'BilibiliCovers')
-    url = f"https://www.bilibili.com/v/popular/{video_type}"
-    covers = bilibili_fetcher.download_bilibili_covers(url)
-    driver.quit()
-    return jsonify(covers)
-
-@app.route('/scrape/sina', methods=['GET'])
-def scrape_sina():
-    news_type = request.args.get('news_type', 'realtime')
-    pages = int(request.args.get('pages', 5))
-    driver = create_driver()
-    folder_name = 'News'
     sina_fetcher = SinaNewsFetcher(driver, folder_name)
-    if news_type == 'realtime':
-        news = sina_fetcher.fetch_realtime_news(max_pages=pages)
-    else:
-        news = sina_fetcher.fetch_trending_news()
+    if option == 'realtime':
+        return sina_fetcher.fetch_realtime_news(max_pages=pages)
+    elif option == 'trending':
+        return sina_fetcher.fetch_trending_news()
     driver.quit()
-    return jsonify(news)
+
+
+def run_github_trending_fetcher(time_range):
+    github_fetcher = GitHubTrendingFetcher()
+    return github_fetcher.get_github_trending(TRENDING_URLS[time_range])
+
+
+def run_bilibili_covers_fetcher(video_type):
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--no-sandbox")
+    driver = webdriver.Chrome(options=chrome_options)
+
+    bilibili_fetcher = BilibiliCoversFetcher(driver, 'BilibiliCovers')
+    if video_type == 'history':
+        return bilibili_fetcher.download_bilibili_covers("https://www.bilibili.com/v/popular/history")
+    elif video_type == 'weekly':
+        return bilibili_fetcher.download_bilibili_covers("https://www.bilibili.com/v/popular/weekly")
+    driver.quit()
+
+
+@app.route('/run_crawler', methods=['POST'])
+def run_crawler():
+    data = request.json
+    crawler_type = data.get('crawler_type')
+    option = data.get('option')
+    pages = data.get('pages', 5)
+
+    if crawler_type == 'sina':
+        result = run_sina_news_fetcher(option, pages)
+    elif crawler_type == 'github':
+        result = run_github_trending_fetcher(option)
+    elif crawler_type == 'bilibili':
+        result = run_bilibili_covers_fetcher(option)
+    else:
+        return jsonify({'error': 'Invalid crawler type'})
+
+    return jsonify(result)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
